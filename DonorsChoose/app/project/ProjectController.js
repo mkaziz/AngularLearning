9da03efe20ad26b2a81a2ea28e424a46695369fb
khalid <kaziz@u.northwestern.edu>
2015-01-12 20:00:16 -0600

    Working project page

--- DonorsChoose/app/project/ProjectController.js
+++ DonorsChoose/app/project/ProjectController.js
@@ -1,7 +1,20 @@
-angular.module("donorsApp").controller("ProjectController", ['$location', 'API', function ($location, API) {
+angular.module("donorsApp").controller("ProjectController", ['$location', 'API', '$routeParams', function ($location, API,  $routeParams) {
     "use strict";
     
     var self = this;
+    var projects = API.getSearchResults();
+
+    var currentProjectArray = projects.proposals.filter(function (proposal) {
+        return proposal.id == $routeParams.id;
+    });
+    
+    if (currentProjectArray.length > 0) {
+        self.project = currentProjectArray[0];
+        console.log(self.project);
+    }
+    else {
+        throw 'project not loaded successfully';
+    }
     
     
 }]);
